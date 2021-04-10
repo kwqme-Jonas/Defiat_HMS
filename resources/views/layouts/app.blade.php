9066e50052f919c4d3aa6529b668b79f27a2e8de
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2021-04-10 19:21:25 +0000

    Installed tailwindcss

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>DefiatHMS</title>
+</head>
+<body>
+    
+</body>
+</html>
